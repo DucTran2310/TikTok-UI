d73021beee83197c1c4ee7239ea832f25caa7aff
Đức Trần <18520021@gm.uit.edu.vn>
2022-05-19 16:16:11 +0700

    Set up router

--- src/App.js
+++ src/App.js
@@ -1,8 +1,41 @@
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import { Fragment } from 'react';
+import { publicRoutes } from './routes/index';
+import { DefaultLayout } from './components/Layout';
+
 function App() {
     return (
-        <div className="App">
-            <h1>React App</h1>
-        </div>
+        <Router>
+            <div className="App">
+                <Routes>
+                    {publicRoutes.map((route, index) => {
+                        // Layout ko co layout duoc xac dinh thi mac dinh la Default Layout
+                        // const Layout = route.layout === null ? Fragment : DefaultLayout;
+                        const Page = route.component;
+
+                        let Layout = DefaultLayout;
+
+                        if (route.layout) {
+                            Layout = route.layout;
+                        } else if (route.layout === null) {
+                            Layout = Fragment;
+                        }
+
+                        return (
+                            <Route
+                                key={index}
+                                path={route.path}
+                                element={
+                                    <Layout>
+                                        <Page />
+                                    </Layout>
+                                }
+                            />
+                        );
+                    })}
+                </Routes>
+            </div>
+        </Router>
     );
 }
 
