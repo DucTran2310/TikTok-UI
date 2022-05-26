0b3810e2ee6572bb75371aec5eb2104c3184d62c
Đức Trần <18520021@gm.uit.edu.vn>
2022-05-26 11:26:21 +0700

    Design btn more

--- src/components/AccountItem/index.js
+++ src/components/AccountItem/index.js
@@ -10,7 +10,7 @@ function AccountItem() {
         <div className={cx('wrapper')}>
             <img
                 className={cx('avatar')}
-                src="https://p16-sign-va.tiktokcdn.com/tos-useast2a-avt-0068-giso/9552a57e0b326fabe47bf018cd48b10d~c5_100x100.jpeg?x-expires=1653444000&x-signature=SkSSPoOFqZJ8mHFbmWLf3BE03Uk%3D"
+                src="https://scontent.fhan4-2.fna.fbcdn.net/v/t1.6435-1/117112375_2718622871746648_7562445710068070383_n.jpg?stp=cp0_dst-jpg_p40x40&_nc_cat=106&ccb=1-7&_nc_sid=7206a8&_nc_eui2=AeHyr_GT9hGHGAasCB1S6uOqZpd7ykdaGqpml3vKR1oaqq5v5ZVbnC84OhuKI_ynfGjl6DcLBU4NAr5NmROUivuX&_nc_ohc=q4bru86pmpgAX8mDRHL&_nc_ht=scontent.fhan4-2.fna&oh=00_AT8CC5kherlnbPdjLIhEp6oGBuhFZt37blwyC9imCgynAQ&oe=62B55317"
                 alt="accountuser"
             />
             <div className={cx('info')}>
